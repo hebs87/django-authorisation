156849f37b3bb9b8304dad4eebf6cac3b0b95f89
Ubuntu <ubuntu@ip-172-31-91-202.ec2.internal>
2019-08-20 21:07:21 +0000

    Added flash message when user logs out

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 # Create your views here.
 def index(request):
@@ -14,5 +14,7 @@ def logout(request):
     Log the user out
     '''
     auth.logout(request)
+    # Display a flash message when user is logged out
+    messages.success(request, "You have successfully been logged out!")
     # Reverse allows to redirect to url name rather than html file or function
     return redirect(reverse('index'))
