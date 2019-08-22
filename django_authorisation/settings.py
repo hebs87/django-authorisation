5f5cd2b8651460f293b9776e0270b15bbc29deed
Ubuntu <ubuntu@ip-172-31-91-202.ec2.internal>
2019-08-22 16:04:07 +0000

    Installed django_forms_bootstrap and styled django forms

--- django_authorisation/settings.py
+++ django_authorisation/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # ------------------------------ EDITED ------------------------------
+    'django_forms_bootstrap',
     'accounts'
 ]
 
