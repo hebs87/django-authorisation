b8e18d84f1417f2470f25d49f7a654246406fb2f
Ubuntu <ubuntu@ip-172-31-91-202.ec2.internal>
2019-08-22 15:15:01 +0000

    Added email sending functionality and reset page templates

--- django_authorisation/settings.py
+++ django_authorisation/settings.py
@@ -128,4 +128,13 @@ STATIC_URL = '/static/'
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # ------------------------------ ADDED ------------------------------
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Remove once tested console email
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# ------------------------------ ADDED ------------------------------
+# Actual email sending config
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
